ef95ebd49606e1e118cfa16c2974158431402f2c
dbojan <dbojan@users.noreply.github.com>
2019-10-15 13:38:29 +0200

    Add files via upload

---
+++ app/src/main/java/com/dbojan/speedometer/MainActivity.java
@@ -0,0 +1,102 @@
+package com.dbojan.speedometer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //added because new access fine location policies, imported class..
+       //ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
+        //check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+        } else {
+
+            //start the program if permission is granted
+            doStuff();
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        TextView txt = (TextView) this.findViewById(R.id.textView1);
+
+        if (location==null){
+
+            txt.setText("-.- km/h");
+        } else {
+            float nCurrentSpeed = location.getSpeed() * 3.6f;
+            txt.setText(String.format("%.2f", nCurrentSpeed)+ " km/h" );
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                doStuff();
+            } else {
+
+                finish();
+            }
+
+        }
+
+    }
+
+
+    private void doStuff(){
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (lm != null){
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+            //commented, this is from the old version
+            // this.onLocationChanged(null);
+        }
+        Toast.makeText(this,"Waiting for GPS connection!", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
